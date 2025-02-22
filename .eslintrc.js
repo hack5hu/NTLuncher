fecf63bb6235c6411cbc91078c0eab98d7d4c97e
hack5hu <priyanshu1kumar24@gmail.com>
2025-02-23 04:15:15 +0530

    fixed issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,11 @@
-import js from '@eslint/js';
-import react from 'eslint-plugin-react';
-import prettier from 'eslint-plugin-prettier';
-import prettierConfig from 'eslint-config-prettier';
-
-export default [
-  js.configs.recommended,
-  react.configs.recommended,
-  prettier.configs.recommended,
-  {
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
-    plugins: {
-      react,
-      prettier,
-    },
-    rules: {
-      'prettier/prettier': 'error',
-      'no-unused-vars': 'warn',
-      'no-console': 'error',
-      'react/prop-types': 'off',
-    },
+module.exports = {
+  root: true,
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+    'no-unused-vars': 'warn',
+    'no-console': ['error', {allow: ['warn', 'error']}], // ❌ Disallow console.log but allow warn & error
+    'react/prop-types': 'off',
   },
-];
+};
