041b7ccb820713352f72d3a6a11b2d543350a18b
hack5hu <priyanshu1kumar24@gmail.com>
2025-02-23 04:06:09 +0530

    fixed issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,26 @@
-module.exports = {
-  root: true,
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:prettier/recommended', // Enables eslint-plugin-prettier
-    'prettier', // Enables eslint-config-prettier
-  ],
-  settings: {
-    react: {
-      version: 'detect', // Auto-detect React version
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+  react.configs.recommended,
+  prettier.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      react,
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'no-unused-vars': 'warn',
+      'no-console': 'error',
+      'react/prop-types': 'off',
     },
   },
-  plugins: ['prettier'],
-  rules: {
-    'prettier/prettier': 'error',
-    'no-unused-vars': 'warn',
-    'no-console': 'error',
-    'react/prop-types': 'off',
-  },
-};
+];
